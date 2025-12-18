c284982e9488df361c4bcea9123c1b3054bfeef1
SebandCo <113468072+SebandCo@users.noreply.github.com>
2025-12-18 22:24:06 +0100

    Correction de debug=True

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
@@ -131,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static",]
+print("BASE_DIR =", BASE_DIR)
+print("STATICFILES_DIRS =", STATICFILES_DIRS)
